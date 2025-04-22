2d70f81f15bc96ed2681f2df63e70446d7ce5d5c
dupont14 <dupontlucian@gmail.com>
2025-04-22 17:42:11 +0000

    commiting python files

--- Task_7/task7.py
+++ Task_7/task7.py
@@ -5,6 +5,33 @@
 # and reports the number of lines and words
 import sys
 import csv
+
+def average(list):
+	length = len(list)
+	sum  = 0
+	for i in range(length):
+		sum = sum + list[i]
+	avg = sum/length
+	return avg
+
+def minimum(list):
+	length = len(list)
+	min = list[0]
+	for i in range(length):
+		if list[i] < min:
+			min = list[i]
+	return min
+
+
+def maximum(list):
+        length = len(list)
+        max = list[0]
+        for i in range(length):
+                if list[i] >  min:
+                        max = list[i]
+        return max
+
+
 infile = open(sys.argv[1])
 reader = csv.reader(infile)
 headings = []
@@ -13,5 +40,7 @@ rows = []
 
 for line in reader:
 	rows.append(float(line[1]))
-	print(rows)
 
+print(average(rows))
+print(min(rows))
+print(max(rows))
